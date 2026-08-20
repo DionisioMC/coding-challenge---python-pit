85f5529efdbf92c8eab8a156133771b43a858b20
Dionisio Coelho <dcoelho@c1r4s6.42porto.com>
2026-08-20 19:13:07 +0100

    Submitting bot

--- Porto_bots/bot_dcoelho_jobeja-n.py
+++ Porto_bots/bot_dcoelho_jobeja-n.py
@@ -1,6 +1,4 @@
-from time import time
-
-OT_NAME = "TR1"
+BOT_NAME = "TR1"
 BOT_COLOR = "#9e0a0a"
 
 MOVES = {
@@ -9,33 +7,43 @@ MOVES = {
     "LEFT": (-1, 0),
     "RIGHT": (1, 0),
 }
+MOVES_REVERSE = list(MOVES.keys())
+MOVES_REVERSE.reverse()
 
 
 def count_open_neighbors(pos, walls, width, height):
     x, y = pos
     count = 0
-    for dx, dy in MOVES.values():
-        nx, ny = x + dx, y + dy
-        if 0 <= nx < width and 0 <= ny < height and (nx, ny) not in walls:
-            count += 1
+    visited = [(x, y)]
+    neighbours = [(x, y)]
+    iter = 3000
+    
+    while neighbours != [] and iter > 0:
+        x, y = neighbours.pop(0)
+        for dx, dy in MOVES.values():
+            nx, ny = x + dx, y + dy
+            if (0 <= nx < width and 0 <= ny < height and (nx, ny) not in walls
+                    and (nx, ny) not in visited):
+                neighbours.append((nx, ny))
+                visited.append((nx, ny))
+                count += 1
+            iter -= 1
     return count
 
 
 def get_move(state):
-    start = time()
     me = state["players"][state["you"]]
     x, y = me["pos"]
     walls = state["walls"]
     width, height = state["width"], state["height"]
 
     best_move, best_score = None, -1
-    for direction, (dx, dy) in MOVES.items():
-        nx, ny = x + dx, y + dy
+    # if y < int(height / 2):
+    for direction in MOVES.keys():
+        nx, ny = x + MOVES[direction][0], y + MOVES[direction][1]
         if not (0 <= nx < width and 0 <= ny < height) or (nx, ny) in walls:
             continue
-        score = count_open_neighbors((nx, ny), walls | {(nx, ny)}, width,
-                                     height)
+        score = count_open_neighbors((nx, ny), walls | {(nx, ny)}, width, height)
         if score > best_score:
             best_move, best_score = direction, score
-    print(time() - start)
     return best_move or "UP"
