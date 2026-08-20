7ca39d4a6c85ae6711bed1e75a17cec5dd63fdcf
Dionisio Coelho <dcoelho@c1r4s6.42porto.com>
2026-08-20 15:59:52 +0100

    start

---
+++ Porto_bots/bot_dcoelho_jobeja-n.py
@@ -0,0 +1,22 @@
+BOT_NAME = "TR1"
+BOT_COLOR = "#9e0a0a"
+
+MOVES = {
+    "UP": (0, -1),
+    "DOWN": (0, 1),
+    "LEFT": (-1, 0),
+    "RIGHT": (1, 0),
+}
+
+
+def get_move(state):
+    me = state["players"][state["you"]]
+    x, y = me["pos"]
+    walls = state["walls"]
+    width, height = state["width"], state["height"]
+
+    for direction, (dx, dy) in MOVES.items():
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < width and 0 <= ny < height and (nx, ny) not in walls:
+            return direction
+    return "UP"
